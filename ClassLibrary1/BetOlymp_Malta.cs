53cdec5ca2ae753fc7e9a5e8b3ce358e79e4f73c
Maksim Kolodijev <m.kolodijev@sauleit.local>
2018-11-30 17:07:32 +0200

    first commit

---
+++ ClassLibrary1/BetOlymp_Malta.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BetOlymp_Malta
+{
+    public class BetOlymp_Malta
+    {
+        private IWebDriver _driver;
+
+        public bool IsElementVisible(IWebElement element)
+        {
+            return element.Displayed && element.Enabled;
+        }
+
+        public bool IsElementDisplayed(IWebElement element) //Unfortunately, not always Displayed == Enabled, so please use appropriate method
+        {
+            return element.Displayed;
+        }
+
+        public void StartFromHomePage()
+        {
+            _driver.Navigate().GoToUrl("http://10.10.0.118:6681/bov2/en/sports/");
+        }
+
+        public void Login()
+        {
+            _driver.FindElement(By.Id("login-username")).Click();
+            _driver.FindElement(By.Id("login-username")).SendKeys("Tester291103");
+            _driver.FindElement(By.Id("login-password")).Click();
+            _driver.FindElement(By.Id("login-password")).SendKeys("Test1234");
+            _driver.FindElement(By.Id("login_btn")).Click();
+        }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            _driver = new ChromeDriver();
+        }
+
+        [Test, Description ("Logo presents and available")]
+        public void HomePage()
+        {
+            StartFromHomePage();
+            IWebElement element = _driver.FindElement(By.ClassName("head_logo_wrp")); //Here we find element by CSS selector
+            Assert.True(IsElementVisible(element));
+        }
+
+        [Test, Description("Base Login")]
+        public void BaseLogin()
+        {
+            StartFromHomePage();
+            Login();
+            IWebElement element = _driver.FindElement(By.ClassName("inl_ico icon-wallet-1")); 
+            Assert.True(IsElementVisible(element));
+            Thread.Sleep(3000);
+            //Assert.True(IsElementVisible(element));
+        }
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_driver != null)
+                _driver.Quit();
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,19 @@
+Basic test suite for testing Arvato Front Page version 1.0
+
+Author: Maksim Kolodijev (maksim.kolodijev@mail.ee / 55581521)
+
+DESCRIPTION:
+
+Arvato Front Page application UI tests set. Chrome Browser is used as a main browser, but IE and FF could be used as well.
+
+REQUIREMENTS:
+
+Visual Studio (was developed using VS 2017, but also should be compatible with VS 2015 CE)
+Selenium Web Driver (https://goo.gl/1ux6yL)
+NUnit3 (http://nunit.org)
+
+TESTS EXECUTION:
+
+1. Open solution with Visual Studio
+2. Build solution
+3. Run application
